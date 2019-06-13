c9f7dbf6c043787ea0257464169ccd286b9f006b
Bindumadireddy <50976730+Bindumadireddy@users.noreply.github.com>
2019-06-13 17:12:20 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -6,25 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  num:any=200;
-  dis:any;
-   Mybutton(event:any)
-   {
-     if(this.num>0)
-     {
-       this.dis=""
-     }
-       this.num=this.num+10;
+  num:string="";
+ 
+  display:string="";
+  count=0;
+  result:any;
+  operators:string="";
+  array=[]
+ 
+  value(numbers){
+    this.num=this.num+numbers;
+    this.display=`${this.num}`;
+  }
+  oper(operations)
+  {
+    if(this.count==0)
+    {
+      this.num=this.num+operations;
+    this.operators=operations;
+    this.count=1;
+    
+    this.display=`${this.num}`;
+  }}
+  evaluate(){
+    this.display=`${this.num}`;
+    this.array=this.num.split(this.operators);
+  
+  switch(this.operators)
+  {
+    case "+":
+            this.result=parseInt(this.array[0])+parseInt(this.array[1])
+            break;
+    case "-":
+            this.result=parseInt(this.array[0])-parseInt(this.array[1])
+            break;
+  case "*":
+            this.result=parseInt(this.array[0])*parseInt(this.array[1])
+            break;
+case "/":
+            this.result=parseInt(this.array[0])/parseInt(this.array[1])
+            break;        
+    }        
+    }
+    clear()
+    {
+      this.num="";
+ 
+      this.display="";
+      this.count=0;
+      result:undefined;
+      this.operators="";
+      this.array=[]
+    }
+  }
 
-   }
-   mybutton(event:any)
-   {
-     if(this.num==10)
-     {
-       this.dis="disabled"
-     }
-       this.num=this.num-10
-     
+  
+ 
 
-   }
-}
+
+
+
+  
+  
+  
+  
+
+
